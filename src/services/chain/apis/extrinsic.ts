5b03ddcdaf00fae6a6c2890eb921cdc7148bffec
mehtaishita <ishitamehta33@outlook.com>
2022-08-16 16:39:40 -0500

    fix

--- src/services/chain/apis/extrinsic.ts
+++ src/services/chain/apis/extrinsic.ts
@@ -8,8 +8,6 @@ import {
 import {DelegateData, DsnpCallback, DsnpErrorCallback, scaleEncodeDelegateData,} from "./common";
 import {SignerPayloadRaw} from "@polkadot/types/types";
 import {KeyringPair} from "@polkadot/keyring/types";
-import {ModelType, PayloadLocation} from "@dsnp/frequency-api-augment/interfaces/schemas/types";
-import { json } from "stream/consumers";
 
 // import { PalletMsaAddProvider } from "@polkadot/types/lookup";
 // import {u8, u64} from "@polkadot/types-codec";
@@ -120,68 +118,52 @@ export const createMsaForProvider = async (callback: DsnpCallback,
         });
 }
 
-export const fetchAllSchemas = async () => {
-    console.log("inside fetch schemas");
+export const fetchAllSchemas = async (): Promise<Array<any>> => {
     const api = requireGetProviderApi();
 
-    const schema_id = 5;
-    // const schemaId = api.rpc.schemas.getLatestSchemaId();
+    const schema_id = 10;
+    // const schemaId = api.rpc.schemas.getLatestSchemaId("None")
     // console.log(schemaId.toString());
 
+    let returnList: Array<any> = [];
     for (let i = 1; i <= schema_id; i++) {
         try {
-            const schema: SchemaResponse  = await (await api.rpc.schemas.getBySchemaId(i)).unwrap();
-            console.log("schmea {}", schema.toJSON());
-        } catch {continue;}
+            const schema = await (await api.rpc.schemas.getBySchemaId(i));
+            if (!schema.isEmpty) {
+                let s = schema.unwrap();
+                console.log("Schema ID %s, %s, Location: %s", s.schema_id, s.model_type, s.payload_location);
+                returnList.concat(s.schema_id);
+            }
+        } catch(e) {
+            console.log ("Error when fetching schemas: {}", e);
+            continue;
+        }
       }
+    return returnList;
 }
 
-export const registerSchema = async () => {
-    console.log("inside register schema");
+export const registerSchema = async (input: string) => {
     const api = requireGetProviderApi();
     const serviceKeys: KeyringPair = requireGetServiceKeys();
 
-    const staticSchema =
-    `{
-        "type": "record",
-        "name": "User",
-        "fields": [
-            {"name": "name", "type": "string"},
-            {"name": "favorite_number", "type": "int"}
-            {"name": "favorite_restaurant", "type": "string"}
-        ]
-    }
-    `
-
-    const extrinsic =  api.tx.schemas.registerSchema(staticSchema, 'AvroBinary' , 'OnChain');
+    const extrinsic =  api.tx.schemas.registerSchema(input, 'AvroBinary' , 'OnChain');
     await extrinsic?.signAndSend(serviceKeys, {nonce: -1});
 
 };
 
-export const addMessage = async (message: string) => {
-    console.log("inside add message");
+export const addMessage = async (message: string, schema_id: number) => {
     const api = requireGetProviderApi();
     const serviceKeys: KeyringPair = requireGetServiceKeys();
 
-    const extrinsic = api.tx.messages.addOnchainMessage(null, 1, message);
+    const extrinsic = api.tx.messages.addOnchainMessage(null, schema_id, message);
     await extrinsic?.signAndSend(serviceKeys, {nonce: -1});
 }
 
-export const getMessages = async () => {
-    console.log("inside get message");
+export const getMessages = async (schema_id: number) => {
     const api = requireGetProviderApi();
 
-    const messages = await api.rpc.messages.getBySchema(5, {from_block: 0, from_index: 0, to_block: 8, page_size: 1});
+    const messages = await api.rpc.messages.getBySchema(schema_id, {from_block: 0, from_index: 0, to_block: 50_000, page_size: 100});
 
     console.log("messages: {}", JSON.stringify(messages.content, null, ' '));
-
-}
-
-export const getConstant = async () => {
-    const api = requireGetProviderApi();
-
-    const lastHeader = await api.rpc.chain.getHeader();
-    console.log("last header: {}", lastHeader);
-    return lastHeader;
 
 }
