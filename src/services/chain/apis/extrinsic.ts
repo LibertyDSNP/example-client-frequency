40b4cf18912d15792134b7a12baccde0be07ea6d
mehtaishita <ishitamehta33@outlook.com>
2022-09-12 11:51:31 -0500

    moving static schemas to componenet

--- src/services/chain/apis/extrinsic.ts
+++ src/services/chain/apis/extrinsic.ts
@@ -166,60 +166,6 @@ export const fetchAllMessages = async (): Promise<Array<MessageResponse>> => {
     return content;
 }
 
-export const staticSchema = avro.Type.forSchema (
-    {
-        type: 'record',
-        name: 'User',
-        fields: [
-            {name: 'nickname', type: 'string'},
-            {name:'favorite_number',type:'int'},
-            {name:'favorite_restaurant',type:'string'}
-        ]
-    }
-)
-
-export const anotherSchema = avro.Type.forSchema (
-
-        {
-            type: "record",
-            name: "AvrosampleNetCore.AccountDetails",
-            fields: [
-              {
-                name: "AccountId",
-                type: "int"
-              },
-              {
-                name: "AccountName",
-                type: "string"
-              },
-              {
-                name: "Accounts",
-                type: [
-                  "null",
-                  {
-                    type: "array",
-                    items: {
-                      type: "record",
-                      name: "AvrosampleNetCore.SubAccounts",
-                      fields: [
-                        {
-                          name: "AccountId",
-                          type: "int"
-                        },
-                        {
-                          name: "AccountType",
-                          type: [ "null", "string" ]
-                        }
-                      ]
-                    }
-                  }
-                ]
-              }
-            ]
-          }
-
-)
-
 export const registerSchema = async (input: string) => {
     const api = requireGetProviderApi();
     const serviceKeys: KeyringPair = requireGetServiceKeys();
