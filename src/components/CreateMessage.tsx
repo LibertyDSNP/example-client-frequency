21299706e1ee320a5b07ebf03aec4c55150704cd
mehtaishita <ishitamehta33@outlook.com>
2022-09-13 13:23:09 -0500

    trying to create a dynamic from from schema json

--- src/components/CreateMessage.tsx
+++ src/components/CreateMessage.tsx
@@ -1,9 +1,19 @@
 import { Button } from "antd";
 import React from "react";
 import { addMessage } from "../services/chain/apis/extrinsic";
-import { staticSchema } from "./RegisterSchema";
+import { staticSchema, staticSchemaJson } from "./RegisterSchema";
+import staticschemaFields from './staticschemaFields.json';
+import { useState, useEffect } from "react";
+import { Element } from './shared/Element';
 
 const CreateMessage = (): JSX.Element => {
+
+    const [elements, setElements] = useState(null);
+
+    useEffect(() => setElements(staticschemaFields[0]),[]);
+
+    // const {fields, page_label} = elements??{};
+
     const submitMessage =  async () => {
         let input =
         {
@@ -14,7 +24,42 @@ const CreateMessage = (): JSX.Element => {
         console.log("submit message func finished?");
     }
 
-    return <Button onClick={submitMessage}>Submit Message</Button>
+    console.log("static schema fields", staticSchemaJson);
+
+    return (
+        <div className="container">
+        <form>
+            <div className="form-group">
+            <label htmlFor="nickname">Enter Nickname:</label>
+                <input
+                    type="text"
+                    className="form-control"
+                    id="nickname"
+                    placeholder="Enter nickname" />
+            </div>
+            <div className="form-group">
+            <label>Enter Favorite Number:</label>
+            <input
+                type="number"
+                className="form-control"
+                id="favoriteNumber"
+                placeholder="0" />
+            </div>
+            <div className="form-group">
+            <label>Enter Favorite Restaurant</label>
+            <input
+                type="text"
+                className="form-control"
+                id="favoriteRestaurant"
+                placeholder="Enter Restaurant name"  />
+            </div>
+            <div>
+                <Button onClick={submitMessage}>Submit Message</Button>
+            </div>
+        </form>
+        </div>
+    )
+
 }
 
 export default CreateMessage;
