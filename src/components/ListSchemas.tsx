27ddc8aaec6c43f81fb07c6bef7358b3d9bc9016
mehtaishita <ishitamehta33@outlook.com>
2022-09-13 15:57:08 -0500

    react table component for schemas

--- src/components/ListSchemas.tsx
+++ src/components/ListSchemas.tsx
@@ -1,4 +1,4 @@
-import { Button } from "antd";
+import { Button, Table } from "antd";
 import React from "react";
 import { fetchAllSchemas } from "../services/chain/apis/extrinsic";
 import './styles.css';
@@ -11,26 +11,32 @@ const ListSchemas = (): JSX.Element => {
         setListOfSchemas(schemas);
     }
 
+    const columns = [
+    {
+        title: 'Schema Id',
+        dataIndex: 'schema_id',
+        key: 'schema_id',
+        },
+        {
+        title: 'Model Type',
+        dataIndex: 'model_type',
+        key: 'model_type',
+        },
+        {
+        title: 'Payload Location',
+        dataIndex: 'payload_location',
+        key: 'payload_location',
+        },
+        {
+        title: 'Model structure',
+        dataIndex: 'model_structure',
+        key: 'model_structure',
+        },
+    ]
+
     return <>
             <Button onClick={listSchemas}>List Schemas</Button>
-            <div>
-            <table>
-                <tr>
-                    <th>ID</th>
-                    <th>Type</th>
-                    <th>Location</th>
-                    <th>Model</th>
-                </tr>
-                {listOfSchemas?.map((sch) =>
-                    <tr>
-                        <td>{sch.id}</td>
-                        <td>{sch.type}</td>
-                        <td>{sch.location}</td>
-                        <td><pre>{sch.modelParsed}</pre></td>
-                    </tr>
-                )}
-            </table>
-            </div>
+            <Table dataSource={listOfSchemas} columns={columns} />
         </>
 }
 
