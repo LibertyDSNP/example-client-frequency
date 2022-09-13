ccb2662e816b8d9e5a01915b3001675c46075816
mehtaishita <ishitamehta33@outlook.com>
2022-09-13 16:53:39 -0500

    list of schemas table - render columns properly

--- src/components/ListSchemas.tsx
+++ src/components/ListSchemas.tsx
@@ -1,42 +1,37 @@
 import { Button, Table } from "antd";
 import React from "react";
 import { fetchAllSchemas } from "../services/chain/apis/extrinsic";
+import { SchemaDetails } from "../services/schema";
+import startCase from 'lodash/startCase';
 import './styles.css';
+import Column from "antd/lib/table/Column";
 
 const ListSchemas = (): JSX.Element => {
-    const [listOfSchemas, setListOfSchemas] = React.useState<Array<any>>([]);
+    const [listOfSchemas, setListOfSchemas] = React.useState<SchemaDetails[]>([]);
 
     const listSchemas = async () => {
         const schemas = await fetchAllSchemas();
         setListOfSchemas(schemas);
     }
 
-    const columns = [
-    {
-        title: 'Schema Id',
-        dataIndex: 'schema_id',
-        key: 'schema_id',
-        },
-        {
-        title: 'Model Type',
-        dataIndex: 'model_type',
-        key: 'model_type',
-        },
-        {
-        title: 'Payload Location',
-        dataIndex: 'payload_location',
-        key: 'payload_location',
-        },
-        {
-        title: 'Model structure',
-        dataIndex: 'model_structure',
-        key: 'model_structure',
-        },
-    ]
-
     return <>
             <Button onClick={listSchemas}>List Schemas</Button>
-            <Table dataSource={listOfSchemas} columns={columns} />
+            <Table dataSource={listOfSchemas}>
+                <Column title= 'Schema Id' dataIndex= 'schema_id' key= 'schema_id' />
+                <Column title= 'Model Type' dataIndex= 'model_type' key= 'model_type'
+                        render={(type: string) => (startCase(type).toString())}/>
+                <Column title= 'Payload Location' dataIndex= 'payload_location' key= 'payload_location'
+                        render={(loc: string) => (startCase(loc).toString())} />
+                <Column
+                    title="Model Structure"
+                    dataIndex="model_structure"
+                    key="model_structure"
+                    render={(sch: JSON) => (
+                        <pre>
+                            {JSON.stringify({sch}, null, 2)}
+                        </pre>
+                    )} />
+            </Table>
         </>
 }
 
