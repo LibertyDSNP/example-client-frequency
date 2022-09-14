94a658a958338df1d4a190eae218c512901e137b
mehtaishita <ishitamehta33@outlook.com>
2022-09-14 14:22:49 -0500

    small table + expand rows
    and also no props definition for schema componenet

--- src/components/ListSchemas.tsx
+++ src/components/ListSchemas.tsx
@@ -6,11 +6,7 @@ import startCase from 'lodash/startCase';
 import './styles.css';
 import Column from "antd/lib/table/Column";
 
-interface schemaProps {
-    callback: (record: SchemaDetails) => void;
-}
-
-const ListSchemas = (props: schemaProps): JSX.Element => {
+const ListSchemas = (props: any): JSX.Element => {
     const [listOfSchemas, setListOfSchemas] = React.useState<SchemaDetails[]>([]);
 
     const listSchemas = async () => {
@@ -20,27 +16,18 @@ const ListSchemas = (props: schemaProps): JSX.Element => {
 
     return <div>
             <Button onClick={listSchemas}>List Schemas</Button>
-            <Table dataSource={listOfSchemas}>
+            <Table dataSource={listOfSchemas} size="small" expandedRowRender={record => <pre>{JSON.stringify(record.model_structure, null, 2)}</pre>}>
                 <Column title= 'Schema Id' dataIndex= 'schema_id' key= 'schema_id' />
                 <Column title= 'Model Type' dataIndex= 'model_type' key= 'model_type'
                         render={(type: string) => (startCase(type).toString())}/>
                 <Column title= 'Payload Location' dataIndex= 'payload_location' key= 'payload_location'
                         render={(loc: string) => (startCase(loc).toString())} />
                 <Column
-                    title="Model Structure"
-                    dataIndex="model_structure"
-                    key="model_structure"
-                    render={(sch: JSON) => (
-                        <pre>
-                            {JSON.stringify({sch}, null, 2)}
-                        </pre>
-                    )} />
-                     <Column
-                        title="Messages"
-                        key="createMessage"
-                        render={(record) => (
-                            <button onClick={() => props.callback(record)}>Create Message</button>
-                    )} />
+                    title="Messages"
+                    key="createMessage"
+                    render={(record) => (
+                        <button onClick={() => props.callback(record)}>Create Message</button>
+                )} />
             </Table>
         </div>
 }
