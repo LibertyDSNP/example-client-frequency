3ee170b37b279624d57c73732292850648530410
mehtaishita <ishitamehta33@outlook.com>
2022-09-16 09:04:17 -0500

    using antd components

--- src/components/CreateMessage.tsx
+++ src/components/CreateMessage.tsx
@@ -1,80 +1,77 @@
+import { Button, Form, Input, InputNumber } from "antd";
 import React, { useState } from "react";
 import { addMessage } from "../services/chain/apis/extrinsic";
 import { SchemaProps } from "../services/types";
 import { staticSchema } from "./RegisterSchema";
 
+
+
 const CreateMessage = (props: SchemaProps): JSX.Element => {
-    const [messagevalues, setMessageValues] = useState({nickname: "", favorite_number: 0, favorite_restaurant: ""});
+    const [form] = Form.useForm();
 
     const example_msg = { nickname:'omar',favorite_number: 6,favorite_restaurant:'Ramen Takeya'};
 
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const name = event.target.name;
-        const value = event.target.value;
-        setMessageValues(values => ({...values, [name]: value }))
-    }
+    const handleSubmit =  async () => {
 
-    const handleChangeNumber = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const name = event.target.name;
-        const value = parseInt(event.target.value);
-        setMessageValues(values => ({...values, [name]: value }))
-    }
-
-    const handleSubmit =  async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
         let isValid = validateMessage();
         if (isValid) {
             submitMessage();
-            setMessageValues({nickname: "", favorite_number: 0, favorite_restaurant: ""});
-            var formEl = document.getElementById("message-form");
-            (formEl as HTMLFormElement)?.reset();
         };
+        form.resetFields();
     }
 
     const validateMessage = (): boolean => {
-        console.dir(messagevalues);
-        const isMsgValid = staticSchema.isValid(messagevalues);
+        console.dir(form.getFieldsValue());
+        const isMsgValid = staticSchema.isValid(form.getFieldsValue());
         console.log("is submited message valid", isMsgValid);
         return isMsgValid;
     }
 
     const submitMessage = async () => {
-        let avroBuffer: Buffer = staticSchema.toBuffer(messagevalues);
+        let avroBuffer: Buffer = staticSchema.toBuffer(form.getFieldsValue());
         console.log("avro buffer: ", avroBuffer);
         await addMessage(avroBuffer, parseInt(props?.schema?.schema_id));
     }
 
     if (props.isVisible)
     {    return (
-            <div className="container">
-            <form onSubmit={handleSubmit} id="message-form">
-                <label>Enter nickname:
-                <input
-                    type="text"
+
+            <Form
+                name="createMessage"
+                form={form}
+                labelCol={{ span: 3 }}
+                wrapperCol={{ span: 6 }}
+                initialValues={{ remember: true }}
+                onFinish={handleSubmit}
+                autoComplete="off"
+            >
+                <Form.Item
+                    label="Nickname"
                     name="nickname"
-                    placeholder="Enter nickname"
-                    value={messagevalues.nickname}
-                    onChange={handleChange} />
-                </label>
-                <label>Enter Favorite Number:
-                <input
-                    type="number"
+                    rules={[{required: true, message: 'Please input the user\'s nickname' }]}>
+                    <Input />
+                </Form.Item>
+
+                <Form.Item
+                    label="Favorite Number"
                     name="favorite_number"
-                    placeholder="0"
-                    value={messagevalues.favorite_number}
-                    onChange={handleChangeNumber} />
-                </label>
-                <label>Enter Favorite Restaurant
-                <input
-                    type="text"
+                    rules={[{required: true, message: 'Please input the user\'s favorite number' }]}>
+                    <InputNumber />
+                </Form.Item>
+
+                <Form.Item
+                    label="Favorite Restaurant"
                     name="favorite_restaurant"
-                    placeholder="Enter Restaurant name"
-                    value={messagevalues.favorite_restaurant}
-                    onChange={handleChange} />
-                </label>
-            <button type="submit">Submit Message</button>
-            </form>
-            </div>
+                    rules={[{required: true, message: 'Please input the user\'s favorite restaurant' }]}>
+                    <Input />
+                </Form.Item>
+
+                <Form.Item wrapperCol={{ offset: 5, span: 10 }}>
+                    <Button type="primary" htmlType="submit">
+                        Submit Message
+                    </Button>
+                </Form.Item>
+            </Form>
         )}
     else return <></>
 }
